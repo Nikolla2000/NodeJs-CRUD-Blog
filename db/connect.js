aa66b0f4cbec2d3dc96edcf1d61087a52553e8cf
Nikola Uzunov <nikollla2000@abv.bg>
2023-06-20 13:53:03 +0300

    Fixed the connection to database and installed dotenv

--- db/connect.js
+++ db/connect.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
+const mongoURL = process.env.MONGO_URL;
 
 const connectDB = (url) => {
   return mongoose.connect(url, {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  });
+    useUnifiedTopology: true
+  })
+  .then(() => {console.log('Successfull connection to database')})
+  .catch((err) => {
+    res.status(500).json({ message: err.message })
+  })
 };
 
 module.exports = connectDB;
+
